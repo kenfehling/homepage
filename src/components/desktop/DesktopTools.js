68d1381c8624a588573b0e4393b859642f4f0fe8
Ken Fehling <ken@androidideas.org>
2017-01-11 17:18:18 -1000

    Got animations working pretty decently

--- src/components/desktop/DesktopTools.js
+++ src/components/desktop/DesktopTools.js
@@ -4,64 +4,91 @@ import { Container, ContainerGroup, HistoryMatch} from 'react-router-nested-hist
 import DesktopToolsHeader from "./DesktopToolsHeader"
 import DesktopToolsMaster from "./DesktopToolsMaster"
 import DesktopToolsDetail from "./DesktopToolsDetail"
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import TransitionGroup from 'react-addons-transition-group'
 import { categories } from '../../constants/tools'
 import styles from './DesktopTools.scss'
-import * as _ from 'lodash'
+import GSAP from 'react-gsap-enhancer'
+import { TweenMax } from 'gsap'
 
-class Page extends Component {
-  componentWillEnter(cb) {
-    // animate stuff, then call cb();
-    const element = findDOMNode(this);
-    cb();
+const createSlideAnimation = (utils, x1, x2) =>
+    TweenMax.fromTo(utils.target, 1,
+        {xPercent: x1},
+        {xPercent: x2, onComplete: utils.options.callback}
+    )
+
+const noAnimation = (utils) =>
+    TweenMax.to(utils.target, 1, {onComplete: utils.options.callback})
+
+const enterStageRight = (utils) => createSlideAnimation(utils, 100, 0)
+const enterStageLeft = (utils) => createSlideAnimation(utils, -100, 0)
+const exitStateRight = (utils) => createSlideAnimation(utils, 0, 100)
+const exitStateLeft = (utils) => createSlideAnimation(utils, 0, -100)
+
+const Page = GSAP()(class extends Component {
+  static contextTypes = {
+    lastAction: PropTypes.string.isRequired
   }
 
-  componentWillLeave(cb) {
-    // animate stuff, then call cb();
-    const element = findDOMNode(this);
-    cb();
-  }
-
-  render() {
-    return this.props.children
-  }
-}
-
-class AnimatedMatch extends Component {
-  getActionClass() {
+  getEnterAnimation() {
     const {lastAction} = this.context
-
-    console.log(lastAction)
-
     switch (lastAction) {
       case 'back':
-      case 'top': return lastAction
-      case 'switch-to-container': return 'switch'
-      default: return 'forward'
+      case 'top': return enterStageLeft
+      case 'switch-to-container': return null
+      default: return enterStageRight
     }
   }
 
+  getLeaveAnimation() {
+    const {lastAction} = this.context
+    switch (lastAction) {
+      case 'back':
+      case 'top': return exitStateRight
+      case 'switch-to-container': return null
+      default: return exitStateLeft
+    }
+  }
+
+  triggerAnimation(animation, callback) {
+    if (animation) {
+      this.addAnimation(animation, {callback})
+    }
+    else {
+      callback()
+    }
+  }
+
+  componentWillEnter(callback) {
+    this.triggerAnimation(this.getEnterAnimation(), callback)
+  }
+
+  componentWillLeave(callback) {
+    this.triggerAnimation(this.getLeaveAnimation(), callback)
+  }
+
   render() {
-    const {component, type} = this.props
+    return <div style={{position: 'absolute', width: '100%', height: '100%'}}>
+      {this.props.children}
+    </div>
+  }
+})
+
+class AnimatedMatch extends Component {
+  render() {
+    const {component} = this.props
     const {activePage} = this.context
-    const actionClass = this.getActionClass()
     return (<HistoryMatch {...this.props} children={({ matched, ...props }) => {
 
       // TODO: Add this to the library?
       const isOnPage = () => matched && activePage.url === props.pathname
 
-      return (<ReactCSSTransitionGroup
-          component="div"
-          className={`transition-group ${type} ${actionClass}`}
-          transitionName="tool"
-          transitionEnter={true}
-          transitionLeave={true}
-          transitionEnterTimeout={0}
-          transitionLeaveTimeout={0}>
+      return (<TransitionGroup
+          component='div'
+          style={{position: 'absolute', width: '100%', height: '100%'}}>
         {isOnPage() && <Page key={props.pathname}>
-          {createElement(component, props)}
-        </Page>}
-      </ReactCSSTransitionGroup>);
+              {createElement(component, props)}
+            </Page>}
+        </TransitionGroup>)
     }} />);
   }
 }
@@ -77,19 +104,21 @@ export default (props) => (
   <div className={styles.container}>
     <Match pattern='/tools' exactly render={() => <Redirect to="/tools/All" />} />
     <ContainerGroup>
-      <DesktopToolsHeader />
-      {categories.map(c => (
-          <Container initialUrl={`/tools/${c}`} key={c} keepHistory={false}
-                      patterns={[`/tools/${regex(c)}`,
-                                 `/tools/${regex(c)}/:tool`]}>
-            <div className="transition-wrapper">
-              <AnimatedMatch pattern={`/tools/${regex(c)}`} type="tools"
-                             exactly component={DesktopToolsMaster} />
-              <AnimatedMatch pattern={`/tools/${regex(c)}/:tool`}
-                             type="detail" component={DesktopToolsDetail} />
-            </div>
-          </Container>
-      ))}
+      <div style={{height: '100%'}}>
+        <DesktopToolsHeader />
+        {categories.map(c => (
+            <Container initialUrl={`/tools/${c}`} key={c} keepHistory={false}
+                        patterns={[`/tools/${regex(c)}`,
+                                   `/tools/${regex(c)}/:tool`]}>
+              <div className="transition-wrapper">
+                <AnimatedMatch pattern={`/tools/${regex(c)}`} type="tools"
+                               exactly component={DesktopToolsMaster} />
+                <AnimatedMatch pattern={`/tools/${regex(c)}/:tool`}
+                               type="detail" component={DesktopToolsDetail} />
+              </div>
+            </Container>
+        ))}
+      </div>
     </ContainerGroup>
   </div>
 )
