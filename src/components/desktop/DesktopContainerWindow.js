be8e7b186b9d73a3dca58a93ed19f44e10928f73
Ken Fehling <ken@androidideas.org>
2017-03-17 10:23:49 -0400

    Fixed window dragging

--- src/components/desktop/DesktopContainerWindow.js
+++ src/components/desktop/DesktopContainerWindow.js
@@ -6,16 +6,17 @@ export default ({children, name, ...windowProps}) => {
   const id = 'desktop_' + name.toLowerCase()
   const path = `/${name.toLowerCase()}`
   return (
-    <DesktopWindow {...windowProps} name={name} children={({isOnTop}) => (
-      <Container name={id}
-                 initialUrl={path}
-                 patterns={[path]}
-                 className={isOnTop ? 'top-window' : 'background-window'}>
+    <DesktopWindow {...windowProps}
+                   name={name}
+                   className='background-window'
+                   topClassName='top-window'
+  >
+      <Container name={id} initialUrl={path} patterns={[path]}>
         <HistoryRoute
           path={path} exact
           children={children instanceof Function ?
               hProps => children({...hProps}) : () => children} />
       </Container>
-    )} />
+    </DesktopWindow>
   )
 }
--- src/components/desktop/DesktopWindow.js
+++ src/components/desktop/DesktopWindow.js
@@ -1,35 +1,47 @@
-import {PropTypes} from 'react'
+import React, {PropTypes, Children, cloneElement} from 'react'
 import Draggable from 'react-draggable'
 import {HistoryWindow} from 'react-router-nested-history'
 import * as _ from 'lodash'
 import styles from './DesktopWindow.scss'
 
+const Bridge = ({className, children, onMouseDown, ...props}) =>
+    cloneElement(Children.only(children),
+      {defaultClassName: className,
+       onMouseDown: onMouseDown,
+       children: cloneElement(Children.only(children.props.children), {
+         ...props,
+       })})
+
 const DesktopWindow = ({name, container='desktop_' + name.toLowerCase(),
                         menuItems, children, fgColor='#000', bgColor='#FFF',
-                        usePadding=true, className=''}) => (
-  <Draggable cancel=".inner-container">
-    <HistoryWindow forName={container}
-                   className={`${styles.container} ${name} ${className}`}>
-     {props => (
-       <div className="inner-container"
-            style={{
-              backgroundColor:bgColor,
-              color:fgColor,
-              padding:usePadding ? '6px 5px' : null
-            }}
-       >
-         {menuItems ?
-           <div className="menu" style={{backgroundColor:fgColor, color:bgColor}}>
-             <div>{name}</div>
-             {_.map(menuItems, item => <div key={item.name}>{item.name}</div>)}
-           </div> : ''}
-         <div className="body" style={{padding: usePadding ? '15px 10px' : null}}>
-         {children instanceof Function ? children(props) : children}
-         </div>
-       </div>
-      )}
-    </HistoryWindow>
-  </Draggable>
+                        usePadding=true, className, topClassName}) => (
+  <HistoryWindow forName={container}
+                 className={`${styles.container} ${name} ${className}`}
+                 topClassName={`${styles.container} ${name} ${topClassName}`}
+  >
+    <Bridge>
+      <Draggable cancel=".inner-container">
+        <div>
+          <div className="inner-container"
+              style={{
+                backgroundColor:bgColor,
+                color:fgColor,
+                padding:usePadding ? '6px 5px' : null
+              }}
+          >
+            {menuItems ?
+              <div className="menu" style={{backgroundColor:fgColor, color:bgColor}}>
+                <div>{name}</div>
+                {_.map(menuItems, item => <div key={item.name}>{item.name}</div>)}
+              </div> : ''}
+            <div className="body" style={{padding: usePadding ? '15px 10px' : null}}>
+              {children instanceof Function ? children() : children}
+            </div>
+          </div>
+        </div>
+      </Draggable>
+    </Bridge>
+  </HistoryWindow>
 )
 
 DesktopWindow.propTypes = {
