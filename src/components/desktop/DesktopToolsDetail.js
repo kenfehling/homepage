038284caa1efe428034ba666c1fad504f6ee6f44
Ken Fehling <ken@androidideas.org>
2017-03-16 23:01:45 -0400

    Designed desktop tools header

--- src/components/desktop/DesktopToolsDetail.js
+++ src/components/desktop/DesktopToolsDetail.js
@@ -1,6 +1,38 @@
-import BaseToolsDetail from '../shared/BaseToolsDetail'
+import React, {Component, PropTypes} from 'react'
+import Helmet from "react-helmet"
+import {BackLink} from 'react-router-nested-history'
+import {renderStars, getTool, replaceLinks, getIcon} from '../../utils/tools'
 import styles from './DesktopToolsDetail.scss'
 
-export default props => (
-    <BaseToolsDetail className={styles.container} {...props} />
-)
+const DesktopToolsDetail = ({match:{params:{tool, category}}}) => {
+  const toolObject = getTool(tool)
+  const {name, fullName, stars, description} = toolObject
+  return (<div className={styles.container} key={name}>
+    <Helmet
+      title={`${!toolObject ? (category || '') : tool || ''}`}
+      titleTemplate="Ken Fehling - %s"
+      defaultTitle="Ken Fehling"
+    />
+    <div className="heading">
+      <div className='back'>
+        <BackLink>
+          {({params:{tool, category}}) => (
+            <div className='link'>
+              <i className="fa fa-chevron-left" />
+              <div className='text'>{tool || category}</div>
+            </div>
+          )}
+        </BackLink>
+      </div>
+      <div className='icon'>{getIcon(toolObject)}</div>
+      <div className="title">{fullName || name}</div>
+      <div className="skill">
+        <div className="label">Skill level:</div>
+        {renderStars(stars)}
+      </div>
+    </div>
+    <div className="body">{replaceLinks(description, category)}</div>
+  </div>)
+}
+
+export default DesktopToolsDetail
