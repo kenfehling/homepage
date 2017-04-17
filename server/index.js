a69de32d92a85413fc4b87c6eeea9b79a5af371d
Ken Fehling <ken@androidideas.org>
2017-04-17 18:05:16 -0400

    Replaced resume with picture & PDF download

--- server/index.js
+++ server/index.js
@@ -11,8 +11,9 @@ import nodemailer from 'nodemailer'
 import mg from 'nodemailer-mailgun-transport'
 import bodyParser from 'body-parser'
 import auth from './mail.json'
+import {RESUME_FILE} from '../src/constants/links'
 
-const buildPath = path.join(__dirname, 'build')
+const buildPath = __dirname
 const app = express()
 const serve = serveStatic(buildPath)
 app.get('*', serve)
@@ -24,53 +25,58 @@ app.use(compression())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post("/contact", function (req, res) {
+const unless = function(path, middleware) {
+  return function(req, res, next) {
+    if (path === req.path) {
+      return next();
+    } else {
+      return middleware(req, res, next);
+    }
+  }
+}
+
+app.post("/api/contact", function (req, res) {
   const from = req.body.from
   const subject = req.body.subject
   const body = req.body.body
-
-  // create transporter object capable of sending email using the default SMTP transport
   const transporter = nodemailer.createTransport(mg(auth))
-
-  console.log(req)
-
   const mailOptions = {
     'h:Reply-To': from,
     from: 'form@kenfehling.com',
     to: 'me@kenfehling.com',
     subject,
     text: body
-
   }
-  // send mail with defined transport object
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log('\nERROR: ' + error+'\n')
       res.status(error.status || 400).json({error})
     } else {
       console.log('\nRESPONSE SENT: ' + info.response+'\n')
-      res.json({ yo: info.response })
+      res.status(200).json({})
     }
   })
 })
 
-app.get('*', (req, res) => {
-  const context = {}
+app.get('/api/' + RESUME_FILE, function (req, res) {
+  res.setHeader('Content-Type', 'application/octet-stream')
+  res.sendFile(path.join(buildPath, RESUME_FILE))
+})
 
+app.use(unless('/api', (req, res) => {
+  const context = {}
   const html = renderToString(
     <HistoryRouter location={req.url} context={context}>
       <App />
     </HistoryRouter>,
   )
-
   if (context.url) {
     return res.redirect(302, context.url)
   }
-
   return res
     .status(context.status || 200)
     .render('index', {html})
-})
+}))
 
 const port = process.env.PORT || 3000
 app.listen(port, console.log(`Server running on port ${port}`))
