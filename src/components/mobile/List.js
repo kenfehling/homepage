751453d5318e9e3c24c86bab3b6a81c58c571a48
Ken Fehling <ken@androidideas.org>
2017-04-21 23:31:49 -0400

    Some small CSS tweaks

--- src/components/mobile/List.js
+++ src/components/mobile/List.js
@@ -1,10 +1,14 @@
 import React, {PropTypes} from 'react'
 import {ScrollArea} from 'react-router-nested-history'
 import ListItem from './ListItem'
+import bowser from 'bowser'
 import * as styles from './List.scss'
 
+const isIosSafari = bowser.mobile && bowser.safari
+const getClassName = () => styles.container + (isIosSafari ? ' ios-safari' : '')
+
 const List = ({items}) => {
-  return (<ScrollArea className={styles.container} vertical={true}>
+  return (<ScrollArea className={getClassName()} vertical={true}>
     {items.map(item => <ListItem key={item.name} {...item} />)}
   </ScrollArea>)
 }
