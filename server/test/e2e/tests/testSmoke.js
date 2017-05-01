78f4eec9b9ff89035aa98330cfde4e0a068f9fb4
Ken Fehling <ken@androidideas.org>
2017-04-30 20:25:56 -0400

    Trying phantomjs

--- server/test/e2e/tests/testSmoke.js
+++ server/test/e2e/tests/testSmoke.js
@@ -5,6 +5,8 @@ export default {
   'Smoke tests': (client) => {
     client
       .url(path())
+      .pause(5000)
+      //.saveScreenshot('./screenshot.png')
       .click('div.dock a:nth-child(1)')
       .assert.containsText('div.Editor a.title:nth-child(7)', 'react')
       .click('div.dock a:nth-child(2)')
