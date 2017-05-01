faf9a2d0dda6b2d3e3f7b199682922b13f21c0a4
Ken Fehling <ken@androidideas.org>
2017-04-30 20:12:46 -0400

    Switching from Nightmare to Nightwatch

---
+++ server/test/e2e/tests/testSmoke.js
@@ -0,0 +1,15 @@
+const PORT = 3000
+const path = (p='') => `http://localhost:${PORT}` + p
+
+export default {
+  'Smoke tests': (client) => {
+    client
+      .url(path())
+      .click('div.dock a:nth-child(1)')
+      .assert.containsText('div.Editor a.title:nth-child(7)', 'react')
+      .click('div.dock a:nth-child(2)')
+      .click('div.Tools:nth-child(1) a.tool:nth-child(3)')
+      .assert.urlEquals(path('/tools/All/React'))
+      .end()
+  }
+}
