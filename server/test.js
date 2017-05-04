28a2f95dcaa99d4efdc1d54d64554568783bd4c7
Ken Fehling <ken@androidideas.org>
2017-05-04 19:44:09 -0400

    Changed docker repo

--- server/test.js
+++ server/test.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
-const childProcess = require('child_process')
-const exec = childProcess.exec
+const exec = require('child_process').exec
 
 const serverProc = exec('npm start &')
 exec('sleep 5; npm run test:e2e', function(error) {
