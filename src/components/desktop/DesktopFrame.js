ab5f555dcf0095a6135c0abe3ad40b0002086ce3
Ken Fehling <ken@androidideas.org>
2017-05-06 22:35:17 -0400

    Added clock dropdown

--- src/components/desktop/DesktopFrame.js
+++ src/components/desktop/DesktopFrame.js
@@ -1,13 +1,12 @@
-import React, {Component, cloneElement} from 'react'
-import PropTypes from 'prop-types'
-import onClickOutside from 'react-onclickoutside'
+import React from 'react'
 import {apps, backgrounds} from '../../constants/desktop'
 import styles from './DesktopFrame.scss'
 import {HeaderLink} from 'react-router-nested-history'
 import Clock from '../shared/Clock'
 import {changeBackground} from '../../actions/UiActions'
 import connect from 'react-redux/es/connect/connect'
-import AnalogClock, { Themes } from 'react-analog-clock'
+import {ApAnalogClock, ApAnalogClockStyle} from 'apeman-react-clock'
+import MenuItem, {SubMenu} from './DropdownMenuItem'
 
 const AppItem = ({name, onClick, container=`desktop_${name.toLowerCase()}`}) => (
   <HeaderLink toContainer={container} onClick={onClick} className='item'>
@@ -33,15 +32,7 @@ const BackgroundItem = connect(
   })
 )(InnerBackgroundItem)
 
-const SubMenu = ({text, items}) => (
-  <MenuItem text={text}
-            className='item'
-            dropdownClassName='submenu'
-            dropdownPosition='right'
-            openOnMouseOver={true}
-            items={items}
-  />
-)
+
 
 const BackgroundSetting = () => (
   <SubMenu text='Background'
@@ -65,118 +56,17 @@ const settings = [
   <BackgroundSetting />
 ]
 
-const Dropdown = ({items, top, left, onClick, className=''}) => (
-  <div className={className + ' dropdown'}
-       style={{top, left}}
-       onClick={onClick}
-  >
-    {items.map((item, i) => (
-      <div key={i}>{cloneElement(item, {onClick})}</div>
-    ))}
-  </div>
-)
-
 const ClockDropdown = ({top, left}) => (
   <div className='dropdown'>
-    <AnalogClock theme={Themes.light} />
+    <ApAnalogClockStyle scoped
+                        width={140}
+                        height={120}
+                        color='#444'
+                        backgroundColor='#F2F2F2'
+    />
+    <ApAnalogClock />
   </div>
 )
-
-class InnerMenuItem extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      left: 0,
-      right: 0,
-      top: 0
-    }
-  }
-
-  show() {
-    this.setState({active: true})
-  }
-
-  hide() {
-    this.setState({active: false})
-  }
-
-  toggle() {
-    this.setState({active: !this.state.active})
-  }
-
-  handleClickOutside(event) {
-    this.hide()
-  }
-
-  calculateDimensions(element) {
-    if (element) {
-      const left = element.offsetLeft
-      const right = element.offsetLeft + element.offsetWidth
-      const top = element.offsetTop
-      if (left > this.state.left || right > this.state.right || top > this.state.top) {
-        this.setState({left, right, top})
-      }
-    }
-  }
-
-  getDropdownPosition() {
-    const {dropdownPosition='bottom'} = this.props
-    const {left, right, top} = this.state
-    if (dropdownPosition === 'bottom') {
-      return {left}
-    }
-    else if (dropdownPosition === 'right') {
-      return {top, left: right}
-    }
-    else {
-      throw new Error(`Position ${dropdownPosition} not supported`)
-    }
-  }
-
-  createDropdown() {
-    const {items, dropdownClassName} = this.props
-    const position = this.getDropdownPosition()
-    return (
-      <Dropdown items={items}
-                className={dropdownClassName}
-                onClick={() => this.hide()}
-                {...position}
-      />
-    )
-  }
-
-  render() {
-    const {icon, text, className='', openOnMouseOver} = this.props
-    const {active} = this.state
-    return (
-      <div className={className + ' ' + (active ? 'active' : '')}
-           onClick={() => this.toggle()}
-           onMouseOver={() => openOnMouseOver && this.show()}
-           ref={(el) => this.calculateDimensions(el)}
-      >
-        <div>
-          {icon && <span className={'icon ' + icon} />}
-          {text && <span className='text'>{text}</span>}
-        </div>
-        {active && this.createDropdown()}
-      </div>
-    )
-  }
-}
-
-const MenuItem = onClickOutside(InnerMenuItem)
-
-MenuItem.propTypes = {
-  icon: PropTypes.string,
-  text: PropTypes.string,
-  items: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
-  dropdownPosition: PropTypes.string,
-  className: PropTypes.string,
-  dropdownClassName: PropTypes.string,
-  openOnMouseOver: PropTypes.bool
-}
 
 const DesktopFrame = () => (
   <div className={styles.container}>
@@ -189,9 +79,11 @@ const DesktopFrame = () => (
         <MenuItem text='Settings' items={settings} />
       </div>
       <div className='right'>
-        <div className='time'>
-          <Clock format='dddd[,] MMMM D [&nbsp;] h:mm A' />
-        </div>
+        <MenuItem text={<Clock format='dddd[,] MMMM D [&nbsp;] h:mm A' />}
+                  dropdownClassName='time'
+                  dropdownAnchor='right'
+                  items={[<ClockDropdown />]}
+        />
       </div>
     </div>
     <div className='left' />
--- src/components/desktop/DropdownMenu.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by ken on 5/6/17.
- */
