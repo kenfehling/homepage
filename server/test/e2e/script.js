de1f980f73c1fc83890825b530c6b0133135fd16
Ken Fehling <ken@androidideas.org>
2017-05-15 02:03:16 -0400

    Fixed nightwatch path

--- server/test/e2e/script.js
+++ server/test/e2e/script.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const exec = require('child_process').exec
 
 const WAIT_FOR_SERVER_START = 10
-const TEST_CMD = '../../node_modules/nightwatch/bin/nightwatch'
+const TEST_CMD = 'node_modules/nightwatch/bin/nightwatch'
 
 const serverProc = exec('npm start &')
 exec(`sleep ${WAIT_FOR_SERVER_START}; ${TEST_CMD}`, function(error) {
