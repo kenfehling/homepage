951a4d4c43251b097175d80359e704a2eab16142
Ken Fehling <me@kenfehling.com>
2022-06-23 22:43:20 -1000

    Trying to fix GitHub dependencies

--- docker/www/build.sh
+++ docker/www/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker build . --tag=kenfehling/homepage --no-cache
-docker push kenfehling/homepage
+# docker build . --tag=kenfehling/homepage --no-cache
+docker buildx build --platform linux/amd64,linux/arm64 --tag=kenfehling/homepage --push . 
+# docker push kenfehling/homepage
