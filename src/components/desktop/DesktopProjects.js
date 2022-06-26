31050c544a4c63da2846ef40b428508aef8c7cf1
Ken Fehling <kenfehling@gmail.com>
2022-06-25 16:30:57 -1000

    Fixed desktop projects url pattern

--- src/components/desktop/DesktopProjects.js
+++ src/components/desktop/DesktopProjects.js
@@ -49,7 +49,7 @@ const DesktopProjects = () => (
   <div className={styles.container}>
     <Container name='desktop_projects'
               initialUrl='/projects'
-              patterns={folders.map(f => `/projects/${f.id}`)}>
+              patterns={['/projects', ...folders.map(f => `/projects/${f.id}`)]}>
         <HistoryRoute path='/projects' exact>
           <Folders items={folders} />
         </HistoryRoute>
